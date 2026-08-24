e22ce6f3bcefec8e61bf52517b561c476ca4dea4
Bahar <baharmansourbeigi@gmail.com>
2026-08-25 02:42:29 +0330

    feat: add Book, Author, and Category models with admin registration. Closes #1

--- books/admin.py
+++ books/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Author, Book, Category
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price", "publish_date")
+
+
+admin.site.register(Author)
+admin.site.register(Category)
--- books/models.py
+++ books/models.py
@@ -1,1 +1,38 @@
-# Create your models here.
+from django.core.validators import MinValueValidator
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+    publish_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        to="Author", on_delete=models.CASCADE, related_name="books"
+    )
+
+    def __str__(self):
+
+        return f"{self.title}"
+
+    class Meta:
+        ordering = "-created_at"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return f"{self.first_name} {self.last_name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return f"{self.name}"
